81cfe73bc4fa49c433d42a38cfd028d6410c8ebe
André Lopes <andrelopes-code@gmail.com>
2024-07-10 12:59:13 -0300

    add __init__ files to packages

--- src/barberq/models.py
+++ src/barberq/models.py
@@ -1,6 +1,7 @@
-from barberq.utils import validators
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from barberq.utils import validators
 
 
 class ReservationStatusEnum(models.IntegerChoices):
