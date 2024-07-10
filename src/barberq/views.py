a37becb1cdb9a63c3b6f16521e50984707e403fc
André Lopes <andrelopes-code@gmail.com>
2024-07-09 22:08:46 -0300

    add landing page view and template

--- src/barberq/views.py
+++ src/barberq/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def home(request: HttpRequest):
+    return render(request, 'barberq/index.html')
+
+
+@login_required
+def reservation(request: HttpRequest):
+    return render(request, 'barberq/reservation.html')
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,7 +1,13 @@
+from barberq import views
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Endpoint for account management (login, logout, etc.)
     path('accounts/', include('django.contrib.auth.urls')),
+    # Index page (landing page) for the app
+    path('', views.home, name='home'),
+    # Endpoint for the reservation page, allows clients to make reservations
+    path('reservations/', views.reservation, name='reservation'),
 ]
